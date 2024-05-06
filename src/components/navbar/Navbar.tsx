75571adfaa47cfdea4cf5720579788c54dbfbbb3
Hanif Omar <hanif.omarr@gmail.com>
2024-05-06 10:21:33 +0800

    feat: add navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+import { Building2, Menu } from "lucide-react";
+import "./navbar.scss";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <nav>
+      <div className="left">
+        <a className="logo">
+          <Building2 size={30} />
+          <span>Whitespace Hub</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contacts</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign In</a>
+        <a href="/" className="register">
+          Sign Up
+        </a>
+        <Menu
+          className="menu-icon"
+          size={50}
+          onClick={() => setOpen((prev) => !prev)}
+        />
+        <div className={open ? "menu-list active" : "menu-list"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contacts</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign In</a>
+          <a href="/">Sign Up</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
