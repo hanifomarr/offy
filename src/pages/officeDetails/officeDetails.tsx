46eee86656118446e3b7492116602aff60946a5f
Hanif Omar <hanif.omarr@gmail.com>
2024-05-20 12:20:43 +0800

    feat: add feature on office details components

--- src/pages/officeDetails/officeDetails.tsx
+++ src/pages/officeDetails/officeDetails.tsx
@@ -1,8 +1,17 @@
-import { MapPin } from "lucide-react";
+import {
+  Bath,
+  HandCoins,
+  LampDesk,
+  MapPin,
+  PawPrint,
+  PencilRuler,
+  Scaling,
+} from "lucide-react";
 import { singlePostData, userData } from "../../dummy";
 
 import "./officeDetails.scss";
 import Slider from "../../components/slider/Slider";
+import Map from "../../components/map/Map";
 
 const OfficeDetails = () => {
   return (
@@ -32,7 +41,84 @@ const OfficeDetails = () => {
         </div>
       </div>
 
-      <div className="features">Fetures</div>
+      <div className="features">
+        <div className="wrapper">
+          <h1>General</h1>
+          <div className="general-container">
+            <div className="item">
+              <PencilRuler />
+              <div className="info">
+                <h2>Utilities</h2>
+                <p>Renter is responsible</p>
+              </div>
+            </div>
+            <div className="item">
+              <PawPrint />
+              <div className="info">
+                <h2>Utilities</h2>
+                <p>Renter is responsible</p>
+              </div>
+            </div>
+            <div className="item">
+              <HandCoins />
+              <div className="info">
+                <h2>Utilities</h2>
+                <p>Renter is responsible</p>
+              </div>
+            </div>
+          </div>
+          <h1>Sizes</h1>
+          <div className="size-container">
+            <div className="size-container">
+              <div className="item">
+                <Scaling />
+                <div className="info">
+                  <h2>80 sqft</h2>
+                </div>
+              </div>
+              <div className="item">
+                <LampDesk />
+                <div className="info">
+                  <h2>80 sqft</h2>
+                </div>
+              </div>
+              <div className="item">
+                <Bath />
+                <div className="info">
+                  <h2>80 sqft</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h1>Nearby Places</h1>
+          <div className="nearby-container">
+            <div className="size-container">
+              <div className="item">
+                <Scaling />
+                <div className="info">
+                  <h2>80 sqft</h2>
+                </div>
+              </div>
+              <div className="item">
+                <LampDesk />
+                <div className="info">
+                  <h2>80 sqft</h2>
+                </div>
+              </div>
+              <div className="item">
+                <Bath />
+                <div className="info">
+                  <h2>80 sqft</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h1>Location</h1>
+          <div className="location-container">
+            <Map />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
