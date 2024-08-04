0be60d5e59bfb943fc4a1c1e0d6f4a426bf85d67
Hanif Omar <hanif.omarr@gmail.com>
2024-08-04 22:37:49 +0800

    refactor(navbar): restrcuture navbar component

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,53 +1,49 @@
-import { useState } from "react";
-
+import { Link, useLocation } from "react-router-dom";
 import { Building2, Menu } from "lucide-react";
-import "./navbar.scss";
-import { Link } from "react-router-dom";
-import { useAuth } from "../../context/authContext";
+
+import Button from "../ui/button/Button";
+import navigationConfig from "../../config/navigationConfig";
 
 const Navbar = () => {
-  const [open, setOpen] = useState(false);
+  const location = useLocation();
 
-  const { currentUser } = useAuth();
   return (
-    <nav>
-      <div className="left">
-        <Link to={"/"} className="logo">
-          <Building2 size={30} />
-          <span>Whitespace Hub</span>
-        </Link>
-        <Link to="/">Home</Link>
-        <Link to="list">List</Link>
-        <Link to="/">Contacts</Link>
+    <nav className="navbar" aria-label="Main Navigation">
+      <Link to={"/"} className="navbar__logo">
+        <Building2 size={30} />
+        <span>offy</span>
+      </Link>
+
+      <ul className="navbar__links">
+        {navigationConfig.mainNavLinks.map((link) => (
+          <li
+            key={link.path}
+            className={`navbar__item ${
+              location.pathname === link.path ? "navbar__item--active" : ""
+            }`}
+          >
+            <Link to={link.path} className="navbar__link">
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div className="navbar__actions">
+        <Button
+          variant="primary"
+          onClick={() => console.log("test")}
+          size="medium"
+        >
+          Rent your space
+        </Button>
+        <Button onClick={() => console.log("test")} size="medium">
+          Sign In
+        </Button>
       </div>
-      <div className="right">
-        {currentUser ? (
-          <div className="user">
-            <img src={currentUser.avatar || "/noavatar.jpg"} alt="" />
-            <span>{currentUser.username}</span>
-            <Link to="/profile" className="profile">
-              {1 > 0 && <div className="notification">{1}</div>}
-              <span>Profile</span>
-            </Link>
-          </div>
-        ) : (
-          <>
-            <Link to="/">Sign In</Link>
-            <Link to="/" className="register">
-              Sign Up
-            </Link>
-          </>
-        )}
-        <div className="menu-icon">
-          <Menu size={50} onClick={() => setOpen((prev) => !prev)} />
-          <div className={open ? "menu-list active" : "menu-list"}>
-            <Link to="/">Home</Link>
-            <Link to="list">List</Link>
-            <Link to="/">Contacts</Link>
-            <Link to="/">Sign In</Link>
-            <Link to="/">Sign Up</Link>
-          </div>
-        </div>
+
+      <div className="navbar__toggle">
+        <Menu size={30} />
       </div>
     </nav>
   );
