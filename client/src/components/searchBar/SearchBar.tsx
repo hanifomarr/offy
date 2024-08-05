3b77205d27f1cc0b37cbf36e961b6d52a7dcc63f
Hanif Omar <hanif.omarr@gmail.com>
2024-08-05 11:31:42 +0800

    refactor(search-bar): change search bar component styling

--- client/src/components/searchBar/SearchBar.tsx
+++ client/src/components/searchBar/SearchBar.tsx
@@ -1,46 +1,42 @@
 import { useState } from "react";
-import "./searchBar.scss";
 import { Search } from "lucide-react";
 
-const types = ["daily", "montly"];
+const types = [
+  { id: "space", label: "Office Space" },
+  { id: "coworking", label: "Coworking" },
+  { id: "event", label: "Event Space" },
+];
 
 const SearchBar = () => {
   const [query, setQuery] = useState({
-    type: "daily",
-    min: 0,
-    max: 10000,
+    id: "space",
   });
 
   const switchTypes = (value: string) => {
-    setQuery((prev) => ({ ...prev, type: value }));
+    setQuery((prev) => ({ ...prev, id: value }));
   };
 
   return (
     <div className="search-bar">
-      <div className="type">
+      <div className="search-bar__type">
         {types.map((type) => (
           <button
-            key={type}
-            onClick={() => switchTypes(type)}
-            className={query.type === type ? "active" : ""}
+            key={type.id}
+            onClick={() => switchTypes(type.id)}
+            className={query.id === type.id ? "active" : ""}
           >
-            {type}
+            {type.label}
           </button>
         ))}
       </div>
-      <form>
-        <input type="text" name="location" placeholder="Search Location" />
-        <input type="number" name="minPrice" min={0} placeholder="Min Price" />
-        <input
-          type="number"
-          name="maxPrice"
-          max={1000000}
-          placeholder="Max Price"
-        />
-        <button>
-          <Search size={24} />
-        </button>
-      </form>
+      <div className="search-bar__fields">
+        <form>
+          <input type="text" name="location" placeholder="Search Location" />
+          <button>
+            <Search size={24} color="white" />
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
